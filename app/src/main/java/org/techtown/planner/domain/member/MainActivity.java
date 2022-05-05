3364d711d77efaa7a037ee7b1d2b6234b6d7d8f8
Owen-Choi <oldstyle482@gmail.com>
2022-05-05 18:50:34 +0900

    객체지향 설계?

--- app/src/main/java/org/techtown/planner/domain/member/MainActivity.java
+++ app/src/main/java/org/techtown/planner/domain/member/MainActivity.java
@@ -1,23 +1,28 @@
-package org.techtown.planner;
+package org.techtown.planner.domain.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.ktx.Firebase;
 
+import org.techtown.planner.R;
+
 public class MainActivity extends AppCompatActivity {
-    // 얘네는 여기다 박아놔라
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    TextView EmailInput, PasswordInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        EmailInput = findViewById(R.id.SignInEmailEditText);
+        PasswordInput = findViewById(R.id.SignInPasswordEditText);
 
     }
 
