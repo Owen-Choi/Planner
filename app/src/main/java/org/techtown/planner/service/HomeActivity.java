9473738c0aa75ff3f3e8678145bbfbebaf75a79e
Owen-Choi <oldstyle482@gmail.com>
2022-05-10 19:23:07 +0900

    로직 수정, 로그인 및 회원가입 구현

---
+++ app/src/main/java/org/techtown/planner/service/HomeActivity.java
@@ -0,0 +1,16 @@
+package org.techtown.planner.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.techtown.planner.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/org/techtown/planner/service/SignUpActivity.java
+++ app/src/main/java/org/techtown/planner/service/SignUpActivity.java
@@ -1,5 +1,6 @@
 package org.techtown.planner.service;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +8,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.techtown.planner.AppConfig;
@@ -15,15 +20,17 @@ import org.techtown.planner.domain.member.Member;
 import org.techtown.planner.domain.member.MemberService;
 
 public class SignUpActivity extends AppCompatActivity {
-    AppConfig appConfig;
-    MemberService memberService;
+//    AppConfig appConfig;
+//    MemberService memberService;
     EditText Email, Password, Nickname;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        appConfig = (AppConfig) getIntent().getSerializableExtra("appConfig");
-        memberService = appConfig.memberService();
+//        appConfig = (AppConfig) getIntent().getSerializableExtra("appConfig");
+//        memberService = appConfig.memberService();
         findViewById(R.id.SignUpButton).setOnClickListener(SignUpButtonClickListener);
         Email = findViewById(R.id.SignUpEmailEditText);
         Password = findViewById(R.id.SignUpPasswordEditText);
@@ -32,10 +39,34 @@ public class SignUpActivity extends AppCompatActivity {
 
     private boolean conditionChecker(String EmailValue, String PasswordValue, String NicknameValue) {
         // 회원가입 조건들. 추가 예정
-        if(EmailValue.length() != 0 && PasswordValue.length() != 0 && NicknameValue.length() != 0)
+        if (EmailValue.length() != 0 && PasswordValue.length() != 0 && NicknameValue.length() != 0)
             return true;
         else
             return false;
+    }
+
+    private void SignUp(Member member) {
+        firebaseAuth.createUserWithEmailAndPassword(member.getEmail(), member.getPw()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.e("SignUp", "계정생성 성공");
+                    // 계정 생성에 성공할 경우만 DB에 정보를 저장.
+                    firebaseFirestore.collection("user").document(member.getNickname()).set(member).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Log.e("SignUp", "회원가입 완료");
+                            } else{
+                                Log.e("SignUp", "회원가입 실패");
+                            }
+                        }
+                    });
+                } else {
+                    Log.e("SignUp", "계정생성 실패");
+                }
+            }
+        });
     }
 
     View.OnClickListener SignUpButtonClickListener = new View.OnClickListener() {
@@ -45,15 +76,17 @@ public class SignUpActivity extends AppCompatActivity {
             EmailValue = Email.getText().toString();
             PasswordValue = Password.getText().toString();
             NicknameValue = Nickname.getText().toString();
-            if(conditionChecker(EmailValue, PasswordValue, NicknameValue)) {
+            if (conditionChecker(EmailValue, PasswordValue, NicknameValue)) {
                 Member member = new Member(EmailValue, PasswordValue, NicknameValue);
-                if(memberService.SignUp(member)) {
-                    Log.e("SignUpActivity", "회원가입 완료");
-                } else {
-                    Log.e("SignUpActivity", "회원가입 실패");
-                }
-            } else {
-                Log.e("SignUpActivity", "회원가입 조건 불충분");
+                SignUp(member);
+//                if(memberService.SignUp(member)) {
+//                    Log.e("SignUpActivity", "회원가입 완료");
+//                } else {
+//                    Log.e("SignUpActivity", "회원가입 실패");
+//                }
+//            } else {
+//                Log.e("SignUpActivity", "회원가입 조건 불충분");
+//            }
             }
         }
     };
