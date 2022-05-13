cc27b991f9b491564f2612f777fb6efee1cbc836
Owen-Choi <oldstyle482@gmail.com>
2022-05-13 20:16:37 +0900

    API 문제 식별

--- app/src/main/java/org/techtown/planner/service/activities/HomeActivity.java
+++ app/src/main/java/org/techtown/planner/service/activities/HomeActivity.java
@@ -1,18 +1,42 @@
 package org.techtown.planner.service.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.techtown.planner.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // 바텀 네비게이션 바 이용해서 프레그먼트로?
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_schedule:
+                        Log.e("temp", "시간표 fragment 띄우기");
+                        return true;
+                    case R.id.menu_group:
+                        Log.e("temp", "그룹 창 띄우기");
+                        return true;
+                    case R.id.menu_myprofile:
+                        Log.e("temp", "프로필 창 띄우기");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/org/techtown/planner/service/activities/schedule_test_activity.java
+++ app/src/main/java/org/techtown/planner/service/activities/schedule_test_activity.java
@@ -22,18 +22,31 @@ public class schedule_test_activity extends AppCompatActivity {
         ArrayList<Schedule> schedules = new ArrayList<Schedule>();
         Schedule schedule = new Schedule();
         schedule.setClassTitle("Data Structure"); // sets subject
+        schedule.setDay(2);     // 요일은 setDay로 조정한다.
         schedule.setClassPlace("IT-601"); // sets place
         schedule.setProfessorName("Won Kim"); // sets professor
         schedule.setStartTime(new Time(10,0)); // sets the beginning of class time (hour,minute)
         schedule.setEndTime(new Time(13,30)); // sets the end of class time (hour,minute)
         schedules.add(schedule);
+
+        Schedule schedule2 = new Schedule();
+        schedule2.setClassTitle("Operating System"); // sets subject
+        schedule2.setDay(0);     // 요일은 setDay로 조정한다.
+        schedule2.setClassPlace("AI-402"); // sets place
+        schedule2.setProfessorName("Yoo Joon"); // sets professor
+        schedule2.setStartTime(new Time(14,0)); // sets the beginning of class time (hour,minute)
+        schedule2.setEndTime(new Time(16,0)); // sets the end of class time (hour,minute)
+        schedules.add(schedule2);
 //.. add one or more schedules
         timetable.add(schedules);
         timetable.setOnStickerSelectEventListener(new TimetableView.OnStickerSelectedListener() {
             @Override
             public void OnStickerSelected(int idx, ArrayList<Schedule> schedules) {
-                Log.e("temp", "time : " + schedules.get(idx).getStartTime().getHour() +
-                        " : " + schedules.get(idx).getStartTime().getMinute());
+//                Log.e("temp", "time : " + schedules.get(idx).getStartTime().getHour() +
+//                        " : " + schedules.get(idx).getStartTime().getMinute());
+                for(int i=0; i<schedules.size(); i++) {
+                    Log.e("temp", "schedule " + i + " : " + schedules.get(i).getStartTime().getHour());
+                }
             }
         });
     }
