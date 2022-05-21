dbe6e7ce07bec19c32dfec70136a1c8fb95b4ed7
Owen-Choi <oldstyle482@gmail.com>
2022-05-21 15:40:27 +0900

    그룹 리스트 샘플 클래스 작성

--- app/src/main/java/org/techtown/planner/service/fragments/GroupAdapter.java
+++ app/src/main/java/org/techtown/planner/service/fragments/GroupAdapter.java
@@ -5,10 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.techtown.planner.R;
+import org.techtown.planner.domain.Group.GroupContent;
 
 import java.util.ArrayList;
 
