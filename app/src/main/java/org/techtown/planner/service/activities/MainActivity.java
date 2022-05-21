d1f9b987ea827d9ffdd041df0522d485d6f4b7a4
Owen-Choi <oldstyle482@gmail.com>
2022-05-21 16:05:31 +0900

    시간표 초기화 확인창 추가

--- app/src/main/java/org/techtown/planner/service/activities/MainActivity.java
+++ app/src/main/java/org/techtown/planner/service/activities/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()) {
-                        StartActivity(GroupScheduleSample.class);
+                        StartActivity(HomeActivity.class);
                     } else {
                         Log.e("SignIn", "로그인 실패");
                     }
