5d425ceeffcd6cbdb5b9151de83bf4b2de9cf23a
Owen-Choi <oldstyle482@gmail.com>
2022-05-26 00:04:52 +0900

    시간 픽스 조건 확인

--- app/src/main/java/org/techtown/planner/service/activities/EachGroupActivity.java
+++ app/src/main/java/org/techtown/planner/service/activities/EachGroupActivity.java
@@ -33,7 +33,6 @@ import org.techtown.planner.domain.schedule.ScheduleInfo;
 import java.util.ArrayList;
 
 public class EachGroupActivity extends AppCompatActivity {
-    public static final int RESULT_OK_ADD = 1;
     public TextView text1;
     private TimetableView GroupTimetable;
     ArrayList<Schedule> available = new ArrayList<>();
@@ -45,6 +44,10 @@ public class EachGroupActivity extends AppCompatActivity {
     static final Time StartTime = new Time(9, 0);
     static final Time EndTime = new Time(20, 0);
     static final int REQUEST_ADD = 1;
+    static final String TAG = "EachGroupActivity";
+
+    // 채영 추가
+    private int [][] check_time = new int[5][21];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +60,13 @@ public class EachGroupActivity extends AppCompatActivity {
         text1.setText(temp);
         GroupTimetable = findViewById(R.id.groupTimetable);
         groupInfo = (GroupContent) intent.getSerializableExtra("groupContent");
-        Log.e("woong", "onCreate : " + groupInfo.getGname());
+
+        for(int i = 0; i < check_time.length; i++){
+            for(int j = 0; j < check_time[i].length; j++){
+                check_time[i][j] = 0;
+            }
+        }
+
         tempInit();
     }
 
@@ -115,7 +124,7 @@ public class EachGroupActivity extends AppCompatActivity {
                                 MemberSchedule.setEndTime(
                                         new Time(scheduleInfo.getEndTimeHour(), scheduleInfo.getEndTimeMinute()));
                                 unavailable.add(MemberSchedule);
-                                Log.e("woong", "onComplete: " + MemberSchedule.getClassTitle());
+                                setArrayValue(MemberSchedule);
                             }
                             GroupTimetable.add(unavailable);
                         }
@@ -128,15 +137,15 @@ public class EachGroupActivity extends AppCompatActivity {
             if(permissionCheck()) {
                 // 방장이라면 시간을 고정할 수 있다.
                 Intent intent = new Intent(EachGroupActivity.this, FixActivity.class);
-                intent.putExtra("mode", RESULT_OK_ADD);
-                startActivityForResult(intent, 0);
+                intent.putExtra("mode", REQUEST_ADD);
+                startActivityForResult(intent, REQUEST_ADD);
             } else {
                 StartToast("권한이 없습니다.");
             }
         }
 
         private boolean permissionCheck() {
-            if(user.equals(groupInfo.getMasterID()))
+            if(user.getUid().equals(groupInfo.getMasterID()))
                 return true;
             return false;
         }
@@ -146,10 +155,74 @@ public class EachGroupActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_ADD) {
             if (resultCode == EditActivity.RESULT_OK_ADD) {
-                ArrayList<Schedule> item = (ArrayList<Schedule>) data.getSerializableExtra("schedules");
+                Schedule schedule = (Schedule)data.getSerializableExtra("schedule");
+                if(checkFixTime(schedule))
+                    StartToast("추가 가능");
+                else
+                    StartToast("추가 불가능");
             }
         }
     }
+
+    //////////////////////////////////////////////////////
+    // 채영 로직
+    private void setArrayValue(Schedule sample){
+        int ch_day = sample.getDay();
+        int start_m = sample.getStartTime().getMinute();
+        int start_h = sample.getStartTime().getHour();
+
+        int end_m = sample.getEndTime().getMinute();
+        int end_h = sample.getEndTime().getHour();
+
+        int start_index, end_index;
+
+        if (start_m == 0){
+            start_index = start_h - 9;
+        } else {
+            start_index = start_h - 8;
+        }
+
+        if (end_m == 0){
+            end_index = end_h - 9;
+        } else {
+            end_index = end_h - 8;
+        }
+
+        for(int i = start_index; i <= end_index; i++){
+            check_time[ch_day][i] = 1;
+        }
+    }
+
+    private boolean checkFixTime(Schedule ss){
+        int group_day = ss.getDay();
+        int g_start_m = ss.getStartTime().getMinute();
+        int g_start_h = ss.getStartTime().getHour();
+        int g_end_m = ss.getEndTime().getMinute();
+        int g_end_h = ss.getEndTime().getHour();
+        boolean check = true;
+
+        int g_start_idx, g_end_idx;
+
+        if (g_start_m == 0){
+            g_start_idx = g_start_h - 9;
+        } else {
+            g_start_idx = g_start_h - 8;
+        }
+
+        if (g_end_m == 0){
+            g_end_idx = g_end_h - 9;
+        } else {
+            g_end_idx = g_end_h - 8;
+        }
+        for(int i = g_start_idx; i <= g_end_idx; i++){
+            if(check_time[group_day][i] == 1){
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+    //////////////////////////////////////////////////////
 
         private void StartToast(String msg) {
             Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
